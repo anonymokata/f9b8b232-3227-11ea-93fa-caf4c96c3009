56b9e8182bb45d21cb79bc54448d1d64933801d7
someone <someone>
2019-12-15 08:51:20 -0500

    new test

--- src/main/java/People/FamilyA.java
+++ src/main/java/People/FamilyA.java
@@ -6,20 +6,31 @@ import java.time.LocalTime;
 public class FamilyA {
  LocalTime start, end;
  int rate = 0;
+ int pivot;
  public FamilyA(){
  this.start = LocalTime.of(16,59);
  this.end = LocalTime.of(4,0);
- ;
+ }
+ private void setPivot(LocalDateTime dateTimeStart, LocalDateTime dateTimeEnd){
+  pivot = LocalDateTime.of(dateTimeStart.getYear(),dateTimeStart.getMonth(),dateTimeStart.getDayOfMonth(),23,dateTimeStart.getMinute()).getHour();
  }
  public int calculateRate(LocalDateTime dateTimeStart, LocalDateTime dateTimeEnd){
+
   if(dateTimeStart.isAfter(LocalDateTime.of(dateTimeStart.toLocalDate(),start))
   && dateTimeEnd.isBefore(LocalDateTime.of(dateTimeEnd.toLocalDate().plusDays(1),end))){
-   int range = dateTimeEnd.getHour() - dateTimeStart.getHour();
-   for (int i = range; i != 0; i--) {
+   int firstRange = pivot - dateTimeStart.getHour();
+   System.out.println(firstRange);
+
+   if(dateTimeStart.getHour() < LocalDateTime.of(dateTimeStart.getYear(),dateTimeStart.getMonth(),dateTimeStart.getDayOfMonth(),23,dateTimeStart.getMinute()).getHour())
+   for(int range = firstRange; range != 0; range--){
+   System.out.println(range);
+    if(range > 0)
     rate+=15;
+    else
+     rate+=20;
+   }
    }
 
-  }
 return rate;
  }
 }
--- src/test/java/FamilyATest.java
+++ src/test/java/FamilyATest.java
@@ -13,5 +13,13 @@ public class FamilyATest {
             .calculateRate(LocalDateTime.of(2019, Month.AUGUST, 21, 19, 0),
                 LocalDateTime.of(2019, Month.AUGUST, 21, 21, 0)));
   }
+  @Test
+  public void PayRateTest1(){
+    FamilyA familyA = new FamilyA();
+    Assert.assertEquals(100,
+        familyA
+            .calculateRate(LocalDateTime.of(2019, Month.AUGUST, 21, 19, 0),
+                LocalDateTime.of(2019, Month.AUGUST, 22, 2, 0)));
+  }
 
 }
